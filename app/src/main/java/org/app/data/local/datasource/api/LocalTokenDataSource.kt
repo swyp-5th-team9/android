980b53dcaab0b411bf2b8287d3444bd5def974a8
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-03 08:52:07 +0900

    [add/#1] local token datasource 추가

---
+++ app/src/main/java/org/app/data/local/datasource/api/LocalTokenDataSource.kt
@@ -0,0 +1,13 @@
+package org.app.data.local.datasource.api
+
+interface LocalTokenDataSource {
+    suspend fun getAccessToken(): String?
+
+    suspend fun getRefreshToken(): String?
+
+    suspend fun setAccessToken(accessToken: String)
+
+    suspend fun setRefreshToken(refreshToken: String)
+
+    suspend fun clearTokens()
+}
