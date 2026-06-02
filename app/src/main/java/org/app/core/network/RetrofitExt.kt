9feb9b2c0cd7dd22851fa5bc95c1b43fdace1927
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-03 08:56:43 +0900

    [add/#1] network 관련 설정 추가

---
+++ app/src/main/java/org/app/core/network/RetrofitExt.kt
@@ -0,0 +1,5 @@
+package org.app.core.network
+
+fun String?.isJsonObject(): Boolean = this?.startsWith("{") == true && this.endsWith("}")
+
+fun String?.isJsonArray(): Boolean = this?.startsWith("[") == true && this.endsWith("]")
