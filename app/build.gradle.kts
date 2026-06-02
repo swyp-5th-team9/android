131ff782fc1a00ff800ebe2099fa70452cebfd78
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-02 11:11:18 +0900

    [init/#1] gradle 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,34 +1,58 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ktlint)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.parcelize)
+    // TODO alias(libs.plugins.google.services)
+}
+
+val properties = Properties().apply {
+    load(project.rootProject.file("local.properties").inputStream())
 }
 
 android {
     namespace = "org.app"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
+    compileSdk = libs.versions.compileSdk
+        .get()
+        .toInt()
+
+    signingConfigs {
+        create("release") {
+            // TODO: Add signing config details
         }
     }
 
     defaultConfig {
         applicationId = "org.app"
-        minSdk = 24
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        minSdk = libs.versions.minSdk
+            .get()
+            .toInt()
+        targetSdk = libs.versions.targetSdk
+            .get()
+            .toInt()
+        versionCode = libs.versions.versionCode
+            .get()
+            .toInt()
+        versionName = libs.versions.versionName.get()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
             )
+            buildConfigField("String", "BASE_URL", "\"${properties.getProperty("prod.base.url") ?: ""}\"")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
@@ -37,6 +61,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
@@ -53,19 +78,37 @@ ktlint {
 }
 
 dependencies {
+// Androidx
+    implementation(libs.bundles.androidx.core)
+    implementation(libs.androidx.datastore.preferences)
+
+// Compose
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.bundles.compose)
+
+// Kotlinx
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.immutable)
+
+// Coil
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network)
+
+// Network
+    implementation(libs.bundles.network)
+
+// DI
+    implementation(libs.bundles.hilt)
+    ksp(libs.hilt.compiler)
+
+// Debug
+    debugImplementation(libs.bundles.debug)
+
+// Test
     testImplementation(libs.junit)
+    androidTestImplementation(libs.bundles.test)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.junit)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
-    debugImplementation(libs.androidx.compose.ui.tooling)
+
+    implementation(libs.timber)
+    implementation(libs.lottie.compose)
 }
