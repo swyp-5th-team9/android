be067cf1d3e8f976f03e8f3681a2133744d4cff3
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-09 11:35:24 +0900

    [feat/#4] PostKakaoLoginResponse dto 구현

---
+++ app/src/main/java/org/app/data/remote/dto/PostKakaoLoginResponse.kt
@@ -0,0 +1,12 @@
+package org.app.data.remote.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PostKakaoLoginResponse(
+    @SerialName("access_token")
+    val accessToken: String?,
+    @SerialName("refresh_token")
+    val refreshToken: String?,
+)
