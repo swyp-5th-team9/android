a8f9cb85dd2487c86c38bb4bd0bad3ffcb136a04
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-10 10:32:33 +0900

    [feat/#5] NaverLoginMapper 구현

---
+++ app/src/main/java/org/app/data/mapper/NaverLoginMapper.kt
@@ -0,0 +1,16 @@
+package org.app.data.mapper
+
+import org.app.data.model.SocialLoginToken
+import org.app.data.remote.dto.PostNaverLoginResponse
+
+fun PostNaverLoginResponse.toNaverLoginToken(): SocialLoginToken {
+    val access = accessToken?.takeIf { it.isNotBlank() }
+        ?: throw IllegalArgumentException("access_token is missing")
+    val refresh = refreshToken?.takeIf { it.isNotBlank() }
+        ?: throw IllegalArgumentException("refresh_token is missing")
+
+    return SocialLoginToken(
+        accessToken = access,
+        refreshToken = refresh,
+    )
+}
