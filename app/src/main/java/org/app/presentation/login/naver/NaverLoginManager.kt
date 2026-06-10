68fa679ee045c957a441b1cb38b37e7cd33766b8
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-10 10:51:12 +0900

    [feat/#5] NaverLoginManager 구현

---
+++ app/src/main/java/org/app/presentation/login/naver/NaverLoginManager.kt
@@ -0,0 +1,44 @@
+package org.app.presentation.login.naver
+
+import android.content.Context
+import com.navercorp.nid.NidOAuth
+import com.navercorp.nid.oauth.util.NidOAuthCallback
+import kotlinx.coroutines.suspendCancellableCoroutine
+import org.app.core.util.suspendRunCatching
+import org.app.presentation.login.SocialLoginManager
+import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+class NaverLoginManager
+    @Inject
+    constructor() : SocialLoginManager {
+        override suspend fun login(context: Context): Result<String> =
+            suspendRunCatching {
+                suspendCancellableCoroutine { continuation ->
+                    val callback =
+                        object : NidOAuthCallback {
+                            override fun onSuccess() {
+                                val token = NidOAuth.getAccessToken()
+                                if (token != null) {
+                                    continuation.resume(token)
+                                } else {
+                                    continuation.resumeWithException(
+                                        IllegalStateException("Naver access token is null"),
+                                    )
+                                }
+                            }
+
+                            override fun onFailure(
+                                errorCode: String,
+                                errorDesc: String,
+                            ) {
+                                continuation.resumeWithException(
+                                    RuntimeException("Naver login failed: $errorCode - $errorDesc"),
+                                )
+                            }
+                        }
+                    NidOAuth.requestLogin(context, callback)
+                }
+            }
+    }
