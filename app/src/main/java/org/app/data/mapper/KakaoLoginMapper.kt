fba32f0e47258426c6ee0d8a795040de04ac4c31
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-10 10:25:06 +0900

    [chore/#4] 패키징 변경

--- app/src/main/java/org/app/data/mapper/KakaoLoginMapper.kt
+++ app/src/main/java/org/app/data/mapper/KakaoLoginMapper.kt
@@ -1,15 +1,15 @@
 package org.app.data.mapper
 
-import org.app.data.model.KakaoLoginToken
+import org.app.data.model.SocialLoginToken
 import org.app.data.remote.dto.PostKakaoLoginResponse
 
-fun PostKakaoLoginResponse.toKakaoLoginToken(): KakaoLoginToken {
+fun PostKakaoLoginResponse.toKakaoLoginToken(): SocialLoginToken {
     val access = accessToken?.takeIf { it.isNotBlank() }
         ?: throw IllegalArgumentException("access_token is missing")
     val refresh = refreshToken?.takeIf { it.isNotBlank() }
         ?: throw IllegalArgumentException("refresh_token is missing")
 
-    return KakaoLoginToken(
+    return SocialLoginToken(
         accessToken = access,
         refreshToken = refresh,
     )
