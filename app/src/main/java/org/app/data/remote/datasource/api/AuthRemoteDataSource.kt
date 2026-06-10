4fe2cd9d2e8a981ebcb64198054af05de74ce972
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-10 11:04:15 +0900

    [feat/#5]  AuthRemoteDataSource 네이버 추가

--- app/src/main/java/org/app/data/remote/datasource/api/AuthRemoteDataSource.kt
+++ app/src/main/java/org/app/data/remote/datasource/api/AuthRemoteDataSource.kt
@@ -2,7 +2,10 @@ package org.app.data.remote.datasource.api
 
 import org.app.data.remote.dto.BaseResponse
 import org.app.data.remote.dto.PostKakaoLoginResponse
+import org.app.data.remote.dto.PostNaverLoginResponse
 
 interface AuthRemoteDataSource {
     suspend fun postKakaoLogin(authorization: String): BaseResponse<PostKakaoLoginResponse>
+
+    suspend fun postNaverLogin(authorization: String): BaseResponse<PostNaverLoginResponse>
 }
