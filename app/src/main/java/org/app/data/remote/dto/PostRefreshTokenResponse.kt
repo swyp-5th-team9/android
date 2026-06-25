12e8bfbfb7b2f2c68ac1088e9eb8d640b8c61232
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-25 16:25:03 +0900

    [feat/#19]  refreshToken: String? 필드 추가

--- app/src/main/java/org/app/data/remote/dto/PostRefreshTokenResponse.kt
+++ app/src/main/java/org/app/data/remote/dto/PostRefreshTokenResponse.kt
@@ -7,4 +7,6 @@ import kotlinx.serialization.Serializable
 data class PostRefreshTokenResponse(
     @SerialName("accessToken")
     val accessToken: String?,
+    @SerialName("refreshToken")
+    val refreshToken: String?,
 )
