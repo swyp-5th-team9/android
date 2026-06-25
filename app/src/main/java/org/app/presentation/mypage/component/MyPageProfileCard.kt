71b802751114467c9aa8dafa6b2879a90dfa009e
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-25 09:17:21 +0900

    [del/#23] 중복 코드 삭제

--- app/src/main/java/org/app/presentation/mypage/component/MyPageProfileCard.kt
+++ app/src/main/java/org/app/presentation/mypage/component/MyPageProfileCard.kt
@@ -6,20 +6,20 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.model.content.CircleShape
 import com.moball.app.R
 import org.app.core.designsystem.component.UrlImage
 import org.app.core.designsystem.theme.MoballTheme
@@ -37,20 +37,14 @@ fun MyPageProfileCard(
         contentAlignment = Alignment.Center,
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Box(
+            UrlImage(
+                url = profileImageUrl.takeIf { !it.isNullOrBlank() } ?: R.drawable.img_profile,
+                contentDescription = "프로필 이미지",
                 modifier = Modifier
                     .size(80.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .background(MoballTheme.colors.backgroundSurface),
-                contentAlignment = Alignment.Center,
-            ) {
-                UrlImage(
-                    url = profileImageUrl.takeIf { !it.isNullOrBlank() } ?: R.drawable.img_profile,
-                    contentDescription = "프로필 이미지",
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop,
-                )
-            }
+                    .background(MoballTheme.colors.backgroundSurface, shape = CircleShape),
+                contentScale = ContentScale.Crop,
+            )
 
             Spacer(modifier = Modifier.height(12.dp))
 
