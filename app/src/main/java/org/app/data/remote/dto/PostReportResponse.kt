c749d1d7c2e55d54c9c8bcdfabdf57154ae7a351
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-29 20:30:44 +0900

    [feat/#32] 제보 API 응답 DTO 추가 (PostReportResponse)

---
+++ app/src/main/java/org/app/data/remote/dto/PostReportResponse.kt
@@ -0,0 +1,9 @@
+package org.app.data.remote.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PostReportResponse(
+    @SerialName("reportId") val reportId: Long,
+)
