aae233515b570e4ee38d26e68f51783120d10ab7
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-29 12:00:02 +0900

    [feat/#20] PubFilterSubRegionChip

---
+++ app/src/main/java/org/app/presentation/home/pubfilter/component/PubFilterSubRegionChip.kt
@@ -0,0 +1,41 @@
+package org.app.presentation.home.pubfilter.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import org.app.core.designsystem.theme.MoballTheme
+import org.app.core.extension.noRippleClickable
+
+@Composable
+fun PubFilterSubRegionChip(
+    label: String,
+    isSelected: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = label,
+        style = MoballTheme.typography.heading7.semibold14,
+        color = MoballTheme.colors.textPrimary,
+        modifier = modifier
+            .clip(RoundedCornerShape(12.dp))
+            .background(
+                if (isSelected) {
+                    MoballTheme.colors.accentTertiaryLight
+                } else {
+                    MoballTheme.colors.backgroundBase
+                },
+            ).border(
+                1.dp,
+                if (isSelected) MoballTheme.colors.accentPrimary else MoballTheme.colors.borderNormal,
+                RoundedCornerShape(12.dp),
+            ).noRippleClickable(onClick)
+            .padding(horizontal = 19.dp, vertical = 12.dp),
+    )
+}
