e2f80264899fcc1e1fd679855efb27f2e0501e95
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-29 19:44:43 +0900

    [feat/#28] PubRepository 인터페이스 및 Impl 추가

---
+++ app/src/main/java/org/app/data/repository/impl/PubRepositoryImpl.kt
@@ -0,0 +1,75 @@
+package org.app.data.repository.impl
+
+import org.app.core.util.suspendRunCatching
+import org.app.data.mapper.toPubDetail
+import org.app.data.mapper.toPubMapItems
+import org.app.data.mapper.toPubPage
+import org.app.data.model.PubMapItem
+import org.app.data.model.PubPage
+import org.app.data.remote.datasource.api.PubRemoteDataSource
+import org.app.data.repository.api.PubRepository
+import org.app.presentation.pubdetail.model.PubDetail
+import javax.inject.Inject
+
+class PubRepositoryImpl
+    @Inject
+    constructor(
+        private val pubRemoteDataSource: PubRemoteDataSource,
+    ) : PubRepository {
+        override suspend fun getPubs(
+            keyword: String?,
+            teamId: Long?,
+            region: String?,
+            facilityCodes: List<String>?,
+            styleCodes: List<String>?,
+            themeCodes: List<String>?,
+            foodCodes: List<String>?,
+            capacityRange: String?,
+            businessStatus: String?,
+            businessDays: List<Int>?,
+            sort: String?,
+            page: Int?,
+            size: Int?,
+        ): Result<PubPage> =
+            suspendRunCatching {
+                pubRemoteDataSource
+                    .getPubs(
+                        keyword,
+                        teamId,
+                        region,
+                        facilityCodes,
+                        styleCodes,
+                        themeCodes,
+                        foodCodes,
+                        capacityRange,
+                        businessStatus,
+                        businessDays,
+                        sort,
+                        page,
+                        size,
+                    ).data
+                    ?.toPubPage() ?: error("data is null")
+            }
+
+        override suspend fun getMapPubs(
+            swLat: Double,
+            swLng: Double,
+            neLat: Double,
+            neLng: Double,
+            teamId: Long?,
+        ): Result<List<PubMapItem>> =
+            suspendRunCatching {
+                pubRemoteDataSource
+                    .getMapPubs(swLat, swLng, neLat, neLng, teamId)
+                    .data
+                    ?.toPubMapItems() ?: error("data is null")
+            }
+
+        override suspend fun getPubDetail(pubId: Long): Result<PubDetail> =
+            suspendRunCatching {
+                pubRemoteDataSource
+                    .getPubDetail(pubId)
+                    .data
+                    ?.toPubDetail() ?: error("data is null")
+            }
+    }
