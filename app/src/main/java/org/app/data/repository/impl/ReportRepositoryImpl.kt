2d27f4bea1731ffbffca71b2d62ee2a6d8b951c8
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-29 20:31:46 +0900

    [feat/#32] ReportRepositoryImpl 추가 (suspendRunCatching)

---
+++ app/src/main/java/org/app/data/repository/impl/ReportRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package org.app.data.repository.impl
+
+import android.net.Uri
+import org.app.core.util.suspendRunCatching
+import org.app.data.mapper.toReportResult
+import org.app.data.model.ReportResult
+import org.app.data.remote.datasource.api.ReportRemoteDataSource
+import org.app.data.repository.api.ReportRepository
+import javax.inject.Inject
+
+class ReportRepositoryImpl
+    @Inject
+    constructor(
+        private val reportRemoteDataSource: ReportRemoteDataSource,
+    ) : ReportRepository {
+        override suspend fun postReport(
+            category: String,
+            content: String,
+            pubId: Long?,
+            imageUris: List<Uri>,
+        ): Result<ReportResult> =
+            suspendRunCatching {
+                reportRemoteDataSource
+                    .postReport(category, content, pubId, imageUris)
+                    .data
+                    ?.toReportResult()
+                    ?: error("data is null")
+            }
+    }
